9ccecabf061ca964e10e861b845d22520412174c
Mark Michalek <mwmichalek@gmail.com>
2018-05-14 16:55:52 -0400

    Started creating controller.

---
+++ Mwm.BeerFactoryV2.Service/ArduinoController.cs
@@ -0,0 +1,19 @@
+﻿using Mwm.BeerFactoryV2.Service.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mwm.BeerFactoryV2.Service {
+    public class ArduinoController {
+
+        public StatusResult RequestStatus() {
+
+            return new StatusResult();
+
+        }
+
+
+    }
+}
---
+++ Mwm.BeerFactoryV2.Service/Dto/StatusResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mwm.BeerFactoryV2.Service.Dto {
+    public class StatusResult {
+
+        public List<PumpResult> PumpResults { get; set; }
+
+        public List<TemperatureResult> TemperatureResults { get; set; }
+
+        public List<KettleResult> KettleResults { get; set; }
+
+
+    }
+}
