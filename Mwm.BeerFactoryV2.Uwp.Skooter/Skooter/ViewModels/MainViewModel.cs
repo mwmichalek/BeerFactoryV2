fbe6c89e05477b236b60723d53883656744f4656
Michalek, Mark <michalek@21cf.com>
2019-07-22 16:53:26 -0400

    Made some changes.

--- Mwm.BeerFactoryV2.Uwp.Skooter/Skooter/ViewModels/MainViewModel.cs
+++ Mwm.BeerFactoryV2.Uwp.Skooter/Skooter/ViewModels/MainViewModel.cs
@@ -73,5 +73,14 @@ namespace Skooter.ViewModels {
             }
         }
 
+        private int _mtSetpoint;
+
+        public int MtSetpoint {
+            get { return _mtSetpoint; }
+            set {
+                SetProperty(ref _mtSetpoint, value);
+            }
+        }
+
     }
 }
